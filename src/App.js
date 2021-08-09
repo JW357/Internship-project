b7e36058d8982a5b7d76bb5e175400e54312dd72
[JW357] <[jakub.wasilewski19@gmail.com]>
2021-08-09 17:34:20 +0200

    nothing relevant

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Typography variant = "h5" className="logo">
           BrandName
         </Typography>
-        {console.log("qqqq")}
+        {console.log("qqwwwwwwq")}
           <Tabs variant="fullWidth">
             <Tab label="Home"/>
             <Tab label="Product"/>
