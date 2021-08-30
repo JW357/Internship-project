bb9c9511b2d4eeab3a09f9be1b4eab61e1cee482
JW357 <82914345+JW357@users.noreply.github.com>
2021-08-30 09:59:18 +0200

    Section 5 (#6)
    * finishing section 1
    
    * Making changes in feature 4, removing word style from classes,  adding next  section
    
    * adding changes, changing names of the styles, removing negative margins, modyfying navbar
    
    * adding features to join button
    
    * adding section 3, renaming classes and merging smaller classes into the bigger one
    
    * Adding header to the section-4 and gathering images from web
    
    * resizing sections
    
    * making grid blocks with icons and buttons
    
    * changing camelcase naming to BEM, adding spaces before curly brackets in styles
    
    * adding space
    
    * adding section-4 with courses, creating blocks with buttons
    
    * creating section 5 - header and two blocks of expert reviews
    
    * additional change
    
    * changed background images to normal images
    
    * nothing relevant
    
    * removing .idea
    
    * displaying icons based on setState and useEffect and adding space
    
    * deleting .idea
    
    * adding empty blocks
    
    Co-authored-by: [JW357] <[jakub.wasilewski19@gmail.com]>

---
+++ src/components/Experts/Experts.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import './style.scss';
+import StarIcon from '@material-ui/icons/Star';
+import StarOutlineIcon from '@material-ui/icons/StarOutline';
+import expert1 from './images/expert1.jpg';
+import expert2 from './images/expert2.jpg';
+
+function Experts() {
+  const [starNumber] = useState(3);
+  const [stars, setStars] = useState([]);
+
+  useEffect(() => {
+    for (let i = 0; i <= starNumber; i += 1) {
+      setStars((prevState) => [...prevState, i]);
+    }
+  }, []);
+
+  return (
+    <section className="experts">
+      <div className="experts__container">
+        <div className="experts__header">
+          <h5>Practice Advice</h5>
+          <h1>Our Experts Teacher</h1>
+          <p>
+            Problems trying to resolve the conflict between
+            <br />
+            the two major realms of Classical physics: Newtonian mechanics
+          </p>
+        </div>
+        <div className="experts-review__box">
+          <div className="expert-review__block">
+            <img src={expert1} className="expert__picture expert__picture--first" alt="expert1" />
+            <p className="review__text">
+              Slate helps you see how many more days
+              <br />
+              you need to work to reach your financial
+              <br />
+              goal for the month and year.
+            </p>
+            <div className="star__rating">
+              {stars.map((key) => <StarIcon className="star__icon" key={key} />)}
+              <StarOutlineIcon className="star__icon" />
+            </div>
+            <p className="name__text">Regina Miles</p>
+            <p className="role__text">Designer</p>
+          </div>
+          <div className="expert-review__block">
+            <img src={expert2} className="expert__picture expert__picture--second" alt="expert2" />
+            <p className="review__text">
+              Slate helps you see how many more days
+              <br />
+              you need to work to reach your financial
+              <br />
+              goal for the month and year.
+            </p>
+            <div className="star__rating">
+              {stars.map((key) => <StarIcon className="star__icon" key={key} />)}
+              <StarOutlineIcon className="star__icon" />
+            </div>
+            <p className="name__text">Regina Miles</p>
+            <p className="role__text">Designer</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Experts;
